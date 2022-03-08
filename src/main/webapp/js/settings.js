f352f27265a7837a7f97e7f235d662bd7a4cb03b
williasf <williasf@stud.ntnu.no>
2022-03-08 09:49:19 +0100

    feat: add 'newUsername' to settings.js

--- src/main/webapp/js/settings.js
+++ src/main/webapp/js/settings.js
@@ -1,28 +1,34 @@
-document.getElementById("newUsername").value = sessionStorage.getItem("username");
-document.getElementById("cancelButton").addEventListener("click", function(){
-    window.location.href = "../app.html";
+document.getElementById("newUsername").value =
+  sessionStorage.getItem("username");
+document.getElementById("cancelButton").addEventListener("click", function () {
+  window.location.href = "../app.html";
 });
 
 /**
  * Makes HTTP PUT request to server for updating username and password
  */
-function editUser (event) {
-    event.preventDefault();
-    let newInformation = {};
+function editUser(event) {
+  event.preventDefault();
+  let newInformation = {
+    username: document.getElementById("newUsername").value,
+    password: document.getElementById("newPassword").value,
+  };
 
-    fetch('../api/user/'+sessionStorage.getItem("userId"), {
-        method: "PUT",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(newInformation)
+  fetch("../api/user/" + sessionStorage.getItem("userId"), {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newInformation),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      if (response === true) {
+        alert("Bruker oppdatert");
+        window.location.href = "../app.html";
+      } else {
+        alert(
+          "Brukernavn eksisterer fra før, vennligst skriv inn et nytt brukernavn"
+        );
+      }
     })
-        .then(response => response.json())
-        .then(response => {
-            if (response === true) {
-                alert("Bruker oppdatert");
-                window.location.href = "../app.html";
-            } else {
-                alert("Brukernavn eksisterer fra før, vennligst skriv inn et nytt brukernavn");
-            }
-        })
-        .catch(error => console.error(error));
+    .catch((error) => console.error(error));
 }
--- src/main/webapp/js/user.js
+++ src/main/webapp/js/user.js
@@ -1,8 +1,10 @@
 window.addEventListener("load", () => {
-    getUsers();
-    getGroupChats();
+  getUsers();
+  getGroupChats();
 });
-document.getElementById("currentUser").appendChild(document.createTextNode(sessionStorage.getItem("username")));
+document
+  .getElementById("currentUser")
+  .appendChild(document.createTextNode(sessionStorage.getItem(username)));
 let usersFromDb = [];
 let groupChats = [];
 
@@ -10,27 +12,27 @@ let groupChats = [];
  * Makes a HTTP GET request to server for all users
  */
 function getUsers() {
-    fetch('../api/user/', {
-        method: "GET",
+  fetch("../api/user/", {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((users) => {
+      createUsernameElements(users);
     })
-        .then(response => response.json())
-        .then(users => {
-            createUsernameElements(users);
-        })
-        .catch(error => console.error(error));
+    .catch((error) => console.error(error));
 }
 
 /**
  * Makes a HTTP GET request to server for all group chats
  */
-function getGroupChats(){
-    fetch('../api/groupchat/user/'+sessionStorage.getItem("userId"), {
-        method: "GET"
+function getGroupChats() {
+  fetch("../api/groupchat/user/" + sessionStorage.getItem("userId"), {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((groupChatsResponse) => {
+      createGroupChatElements(groupChatsResponse);
+      groupChatsResponse.map((groupChat) => groupChats.push(groupChat));
     })
-        .then(response => response.json())
-        .then(groupChatsResponse => {
-            createGroupChatElements(groupChatsResponse);
-            groupChatsResponse.map(groupChat => groupChats.push(groupChat));
-        })
-        .catch(error => console.error(error));
+    .catch((error) => console.error(error));
 }
